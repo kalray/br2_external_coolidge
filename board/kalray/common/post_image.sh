b6d291532c62f745801180231314ac6b5b967ef4
Clement Leger <clement.leger@kalray.eu>
2019-04-04 09:47:50 +0200

    Use k1-linux-objcopy instead of k1-linux-buildroot
    Summary:
    In order to work with both buildroot compiled toolchain and external
    toolchain, use k1-linux-objcopy which is available for both.
    
    Test Plan: Run the build of all configurations
    
    Reviewers: O53 buildroot!, O51 linux!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D660

--- board/kalray/common/post_image.sh
+++ board/kalray/common/post_image.sh
@@ -2,4 +2,4 @@
 
 echo "Generating vmlinux_l2"
 
-k1-buildroot-linux-uclibc-objcopy --update-section .rm_firmware=${TARGET_DIR}/bin/l2_cache_bin ${BINARIES_DIR}/vmlinux ${BINARIES_DIR}/vmlinux_l2
+k1-linux-objcopy --update-section .rm_firmware=${TARGET_DIR}/bin/l2_cache_bin ${BINARIES_DIR}/vmlinux ${BINARIES_DIR}/vmlinux_l2
