654b167b6aedb7a973ee313da9a3fa24768556a0
Marius Gligor <mgligor@kalray.eu>
2019-04-12 10:11:23 +0200

    Add HOST_GDB_OVERRIDE_SRCDIR path
    Summary:
    In order to build k1-linux-gdb for the host, the path where
    the gdb source can be found is required
    
    Test Plan: Tested by debugging a K1C Linux user application
    
    Reviewers: O53 buildroot!, O51 linux!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D710

--- local.mk
+++ local.mk
@@ -2,6 +2,7 @@ BINUTILS_OVERRIDE_SRCDIR = $(TOPDIR)/../extra_clones/rdtools-linux/gdb
 GCC_INITIAL_OVERRIDE_SRCDIR = $(TOPDIR)/../extra_clones/rdtools-linux/gcc
 GCC_FINAL_OVERRIDE_SRCDIR = $(TOPDIR)/../extra_clones/rdtools-linux/gcc
 GDB_OVERRIDE_SRCDIR = $(TOPDIR)/../extra_clones/rdtools-linux/gdb
+HOST_GDB_OVERRIDE_SRCDIR = $(TOPDIR)/../extra_clones/rdtools-linux/gdb
 UCLIBC_OVERRIDE_SRCDIR = $(TOPDIR)/../uClibc-ng
 LINUX_HEADERS_OVERRIDE_SRCDIR = $(TOPDIR)/../linux
 LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../linux
