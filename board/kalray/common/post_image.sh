d0bbbb5b9ed98be582c13a7c17b1ed11323d1e1d
Clement Leger <cleger@kalray.eu>
2019-04-24 17:35:23 +0200

    Modify l2 firmware to be k1c_firmware
    Summary:
    This package will also contains ethernet firmware to be installed in
    target. At the moment, only the l2_cache binary is present and will be
    used by post-image script.
    The idea is to deliver a package that contains firmware to be used with
    linux.
    
    Test Plan: Build buildroot completely to check that everything is building.
    
    Reviewers: O53 buildroot!, O51 linux!
    
    Differential Revision: https://phab.kalray.eu/D764

--- board/kalray/common/post_image.sh
+++ board/kalray/common/post_image.sh
@@ -2,4 +2,4 @@
 
 echo "Generating vmlinux_l2"
 
-k1-linux-objcopy --update-section .rm_firmware=${TARGET_DIR}/bin/l2_cache_bin ${BINARIES_DIR}/vmlinux ${BINARIES_DIR}/vmlinux_l2
+k1-linux-objcopy --update-section .rm_firmware=${BUILD_DIR}/k1c-firmware/l2_cache_bin ${BINARIES_DIR}/vmlinux ${BINARIES_DIR}/vmlinux_l2
---
+++ package/kalray/k1c-firmware/k1c-firmware.mk
@@ -0,0 +1,10 @@
+################################################################################
+#
+# k1c_firmware
+#
+################################################################################
+
+K1C_FIRMWARE_SITE = $(TOPDIR)/../devimage/firmware_pkg
+K1C_FIRMWARE_SITE_METHOD = local
+
+$(eval $(generic-package))
