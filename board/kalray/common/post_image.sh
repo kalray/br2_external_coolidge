db206226c985e0a093e0384298f0028b87d392ed
Clement Leger <cleger@kalray.eu>
2019-04-25 13:06:58 +0200

    post_image.sh: Add missing check for firmware size
    Summary:
    Add the missing code which was checking that the firmware size was
    fitting in the vmlinux elf file.
    
    Test Plan: Ran buildroot and checked that the check is correctly done.
    
    Reviewers: O53 buildroot!, O51 linux!
    
    Differential Revision: https://phab.kalray.eu/D768

--- board/kalray/common/post_image.sh
+++ board/kalray/common/post_image.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
+set -eu
 
 echo "Generating vmlinux_l2"
 
-k1-linux-objcopy --update-section .rm_firmware=${BUILD_DIR}/k1c-firmware/l2_cache_bin ${BINARIES_DIR}/vmlinux ${BINARIES_DIR}/vmlinux_l2
+vmlinux=${BINARIES_DIR}/vmlinux
+l2_cache_bin=${BUILD_DIR}/k1c-firmware/l2_cache_bin
+section=".rm_firmware"
+
+rm_fw_sec_size=$(k1-linux-readelf -W --sections $vmlinux | grep $section | grep '[[:space:]]*\['  | sed -e 's/\[.*\]//g'|awk '{print $5}')
+
+l2_cache_size=$(stat --printf="%s" $l2_cache_bin)
+
+if [ "$rm_fw_sec_size" -lt "$rm_fw_sec_size" ]; then
+	echo "L2 Firmware is too big for vmlinux"
+	exit 1
+fi
+
+k1-linux-objcopy --update-section $section=${l2_cache_bin} ${vmlinux} ${BINARIES_DIR}/vmlinux_l2
+
