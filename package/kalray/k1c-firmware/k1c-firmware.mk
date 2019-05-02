a8a16f3c66558f63660b203ae4c5a29c4aec5b58
Benjamin Mugnier <bmugnier@kalray.eu>
2019-05-02 17:17:05 +0200

    Handle DMA firmwares in k1c-firmwares
    Summary: Copy dma fw to /lib/firmware rootfs
    
    Test Plan: DMA engine test driver
    
    Reviewers: O53 Buildroot Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D794

--- package/kalray/k1c-firmware/k1c-firmware.mk
+++ package/kalray/k1c-firmware/k1c-firmware.mk
@@ -7,4 +7,9 @@
 K1C_FIRMWARE_SITE = $(TOPDIR)/../devimage/firmware_pkg
 K1C_FIRMWARE_SITE_METHOD = local
 
+define K1C_FIRMWARE_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/lib/firmware/
+	cp -a $(@D)/*.bin $(TARGET_DIR)/lib/firmware/
+endef
+
 $(eval $(generic-package))
