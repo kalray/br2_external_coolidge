557baffe77ade463b9fd0cf02ca9be33d0a210c9
Yann Sionneau <ysionneau@kalray.eu>
2019-06-14 16:51:11 +0200

    Add support for module testing
    Summary: Add support for module testing.
    
    Test Plan: This is the test.
    
    Reviewers: O53 Buildroot Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D1210

--- package/kalray/coolval/coolval.mk
+++ package/kalray/coolval/coolval.mk
@@ -6,7 +6,7 @@
 
 COOLVAL_SITE = $(TOPDIR)/../coolval
 COOLVAL_SITE_METHOD = local
-
+COOLVAL_MODULE_SUBDIRS = src/module_tests
 
 define COOLVAL_BUILD_CMDS
 	CROSS_COMPILE=$(TARGET_CROSS) $(MAKE) -C $(@D)
@@ -20,4 +20,5 @@ define COOLVAL_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 $(COOLVAL_PKGDIR)/S30coolval $(TARGET_DIR)/etc/init.d/
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
