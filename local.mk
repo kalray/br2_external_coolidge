27b17f4a421af11b010eb2fae70e0ee83926e533
Clement Leger <cleger@kalray.eu>
2019-12-10 11:35:46 +0100

    local.mk: Use path as described in the documentation
    Summary:
    In order to allow the user rebuilding an image using the documentation
    and with as few modification as possible, add path as described in
    documentation. If the user follows the userguide, it will be able to
    rebuild an image easily.
    
    Depends On D2883
    
    Test Plan: Rebuild buildroot using new configs
    
    Reviewers: O53 Buildroot Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D2884

--- local.mk
+++ local.mk
@@ -1,12 +1,12 @@
-BINUTILS_OVERRIDE_SRCDIR = $(TOPDIR)/../workspace/extra_clones/csw-linux/gdb
-GCC_INITIAL_OVERRIDE_SRCDIR = $(TOPDIR)/../workspace/extra_clones/csw-linux/gcc
-GCC_FINAL_OVERRIDE_SRCDIR = $(TOPDIR)/../workspace/extra_clones/csw-linux/gcc
-GDB_OVERRIDE_SRCDIR = $(TOPDIR)/../workspace/extra_clones/csw-linux/gdb
-HOST_GDB_OVERRIDE_SRCDIR = $(TOPDIR)/../workspace/extra_clones/csw-linux/gdb
-UCLIBC_OVERRIDE_SRCDIR = $(TOPDIR)/../uClibc-ng
+BINUTILS_OVERRIDE_SRCDIR = $(TOPDIR)/../gdb
+GCC_INITIAL_OVERRIDE_SRCDIR = $(TOPDIR)/../gcc
+GCC_FINAL_OVERRIDE_SRCDIR = $(TOPDIR)/../gcc
+GDB_OVERRIDE_SRCDIR = $(TOPDIR)/../gdb
+HOST_GDB_OVERRIDE_SRCDIR = $(TOPDIR)/../gdb
+UCLIBC_OVERRIDE_SRCDIR = $(TOPDIR)/../uclibc-ng
 LINUX_HEADERS_OVERRIDE_SRCDIR = $(TOPDIR)/../linux
 LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../linux
 BAREBOX_OVERRIDE_SRCDIR = $(TOPDIR)/../barebox
 STRACE_OVERRIDE_SRCDIR = $(TOPDIR)/../strace
 # Override to add "-custom" suffix to k1c-firmware build directory
-K1C_FIRMWARE_OVERRIDE_SRCDIR = $(TOPDIR)/../workspace/devimage/firmware_pkg
+K1C_FIRMWARE_OVERRIDE_SRCDIR = $(TOPDIR)/../k1c-linux-firmware
