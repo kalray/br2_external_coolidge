454b8fcd31bab063d5bde4128c982af7964d6426
Clement Leger <cleger@kalray.eu>
2020-06-02 09:20:10 +0200

    packages/pocl: remove dependency on firmware and add default firmware
    Summary:
    In order to make it possible to build OpenCL application with the
    prebuitl toolchain but without bloating the final image, disable the
    dependency between pocl and pocl-firmware. pocl build will be enabled in
    the standard image and a default firmware will be included allowing
    to build and run an OpenCL application.
    POCL_FIRMWARE configuration has also been modified to select only
    a set of firmwares to be installed.
    
    Ref T13347
    
    Test Plan:
    Compile image and verify that opencl tests are now buildable and
    runnable.
    
    Reviewers: O53 Buildroot Coolidge, gthouvenin
    
    Reviewed By: O53 Buildroot Coolidge, gthouvenin
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T13347
    
    Differential Revision: https://phab.kalray.eu/D6465

--- package/kalray/pocl-firmware/pocl-firmware.mk
+++ package/kalray/pocl-firmware/pocl-firmware.mk
@@ -19,7 +19,14 @@ endif
 
 define POCL_FIRMWARE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/firmware/kalray/opencl/
-	cp -a $(@D)/*.elf $(TARGET_DIR)/lib/firmware/kalray/opencl/
+	if [ -z $(BR2_PACKAGE_POCL_FIRMWARE_LIST) ]; then \
+		cp -a $(@D)/*.elf $(TARGET_DIR)/lib/firmware/kalray/opencl/; \
+	else \
+		$(foreach fw, $(call qstrip,$(BR2_PACKAGE_POCL_FIRMWARE_LIST)), \
+			cp -a $(@D)/$(fw).elf $(TARGET_DIR)/lib/firmware/kalray/opencl/; \
+		) \
+		true; \
+	fi
 endef
 
 $(eval $(generic-package))
