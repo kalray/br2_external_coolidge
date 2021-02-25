9feae3a608ce47cde73cd74558da911dc92348fb
Jerome Reybert <jerome.reybert@kalray.eu>
2021-02-25 10:59:05 +0100

    mppa-rproc: adapt build and add new binary install
    Summary:
    In order to ad cluster-spawner at csw/mppa_rproc stage, mppa_rproc
    source tree has been updated (see D11326)
    
    cluster-spawner is installed in target /usr/bin/
    
    refs T15870
    
    Test Plan: NA
    
    Reviewers: O53 Buildroot Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T15870
    
    Differential Revision: https://phab.kalray.eu/D11382

--- package/kalray/mppa-rproc/mppa-rproc.mk
+++ package/kalray/mppa-rproc/mppa-rproc.mk
@@ -21,16 +21,17 @@ MPPA_RPROC_OPTS += HOST_BACKEND=direct
 MPPA_RPROC_INSTALL_STAGING = YES
 
 define MPPA_RPROC_BUILD_CMDS
-	$(MAKE) -C $(@D)/lib $(MPPA_RPROC_OPTS) all
+	$(MAKE) -C $(@D)/src $(MPPA_RPROC_OPTS) all
 endef
 
 define MPPA_RPROC_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/lib/include/mppa_rproc.h $(STAGING_DIR)/usr/include/mppa_rproc.h
-	$(INSTALL) -D -m 0755 $(@D)/lib/output/lib/cluster/libmppa_rproc_host.so $(STAGING_DIR)/usr/lib/libmppa_rproc_host.so
+	$(INSTALL) -D -m 0644 $(@D)/src/include/mppa_rproc.h $(STAGING_DIR)/usr/include/mppa_rproc.h
+	$(INSTALL) -D -m 0755 $(@D)/src/output/lib/cluster/libmppa_rproc_host.so $(STAGING_DIR)/usr/lib/libmppa_rproc_host.so
 endef
 
 define MPPA_RPROC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/lib/output/lib/cluster/libmppa_rproc_host.so $(TARGET_DIR)/usr/lib/libmppa_rproc_host.so
+	$(INSTALL) -D -m 0755 $(@D)/src/output/lib/cluster/libmppa_rproc_host.so $(TARGET_DIR)/usr/lib/libmppa_rproc_host.so
+	$(INSTALL) -D -m 0755 $(@D)/src/output/bin/cluster-spawner $(TARGET_DIR)/usr/bin/cluster-spawner
 endef
 
 $(eval $(generic-package))
