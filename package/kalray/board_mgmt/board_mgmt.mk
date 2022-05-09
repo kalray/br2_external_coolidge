6527ad9cebae8640a114143239495043f27dc4d6
Julian Vetter <jvetter@kalrayinc.com>
2022-05-09 16:53:38 +0200

    Updated board_mgmt package with new kvx-board-flash and kvx-board-image
    Summary:
    Added new binaries to board_mgmt package.
    
    Ref T18681
    
    Test Plan: TBD
    
    Reviewers: ysionneau, O53 Buildroot Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T18681
    
    Differential Revision: https://phab.kalray.eu/D20614

--- package/kalray/board_mgmt/board_mgmt.mk
+++ package/kalray/board_mgmt/board_mgmt.mk
@@ -16,20 +16,28 @@ BOARD_MGMT_LFLAGS_EXTRA ="kvx-board-mgmt-lflags := -L$(TARGET_DIR)/usr/lib \
                                                    -lmppabm \
                                                    -lreadline \
                                                    -lncurses"
+BOARD_FLASH_LFLAGS_EXTRA ="kvx-board-flash-lflags := -L$(TARGET_DIR)/usr/lib \
+                                                     -lmppabm"
 
 define BOARD_MGMT_BUILD_CMDS
 	$(MAKE) O=$(@D)/output -C $(@D) CC="$(TARGET_CC)" LD="$(TARGET_LD)" \
 		WITH_MPSSE_IF=0 $(BOARD_MGMT_LFLAGS_EXTRA) kvx-board-mgmt
+	$(MAKE) O=$(@D)/output -C $(@D) CC="$(TARGET_CC)" LD="$(TARGET_LD)" \
+		WITH_MPSSE_IF=0 $(BOARD_FLASH_LFLAGS_EXTRA) kvx-board-flash
 endef
 
 define BOARD_MGMT_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/utils/kvx-board-diag $(STAGING_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/utils/kvx-board-image $(STAGING_DIR)/usr/bin/
 	$(INSTALL) -D -m 0755 $(@D)/output/bin/kvx-board-mgmt $(STAGING_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/output/bin/kvx-board-flash $(STAGING_DIR)/usr/bin/
 endef
 
 define BOARD_MGMT_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/utils/kvx-board-diag $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/utils/kvx-board-image $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 0755 $(@D)/output/bin/kvx-board-mgmt $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/output/bin/kvx-board-flash $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
