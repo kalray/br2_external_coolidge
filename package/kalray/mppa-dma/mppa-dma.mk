52d7a144f657df1598c609092790dc99d51b3012
Julian Vetter <jvetter@kalrayinc.com>
2022-07-26 15:44:56 +0200

    mppa-dma: Removed copy of device headers
    Summary:
    The package machine_headers already provides the according header files.
    So, no need to copy them or reference anything inside kEnv. Everything
    is now installed in $(STAGING_DIR)/usr/include/...
    
    Ref T19412
    
    Test Plan: CI
    
    Reviewers: ysionneau, jmaselbas, O53 Buildroot Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T19412
    
    Differential Revision: https://phab.kalray.eu/D21868

--- package/kalray/mppa-dma/mppa-dma.mk
+++ package/kalray/mppa-dma/mppa-dma.mk
@@ -6,7 +6,7 @@
 
 MPPA_DMA_WORKSPACE_PATH = $(TOPDIR)/../workspace
 MPPA_DMA_SITE = $(MPPA_DMA_WORKSPACE_PATH)/extra_clones/csw-linux/mppa_dma/libs/mppa
-MPPA_DMA_DEVICE_HEADERS_PATH = $(MPPA_DMA_WORKSPACE_PATH)/kEnv/kvxtools/opt/kalray/accesscore/kvx-elf/include/machine/devices
+MPPA_DMA_MACHINE_HEADERS_PATH = $(STAGING_DIR)/usr/include/machine
 MPPA_DMA_SITE_METHOD = local
 ifeq ($(BR2_MPPA_DMA_CUSTOM_TARBALL),y)
 undefine MPPA_DMA_SITE_METHOD
@@ -25,24 +25,21 @@ endif
 MPPA_DMA_INSTALL_STAGING = YES
 
 define MPPA_DMA_BUILD_CMDS
-	$(MAKE) -C $(@D) TOOLCHAIN=linux DEVICE_HEADERS=$(MPPA_DMA_DEVICE_HEADERS_PATH) $(MPPA_DMA_OPTS) all V=2
+	$(MAKE) -C $(@D) TOOLCHAIN=linux \
+			 MACHINE_HEADERS=$(MPPA_DMA_MACHINE_HEADERS_PATH) \
+			 $(MPPA_DMA_OPTS) all V=2
 endef
 
 define MPPA_DMA_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/include/mppa_dma.h $(STAGING_DIR)/usr/include/mppa_dma.h
-	$(INSTALL) -D -m 0644 $(@D)/include/mppa_dma_pcie.h $(STAGING_DIR)/usr/include/mppa_dma_pcie.h
-	$(INSTALL) -D -m 0644 $(@D)/include/arch/linux/mppa_dma_macros.h $(STAGING_DIR)/usr/include/mppa_dma_macros.h
-	$(INSTALL) -D -m 0644 $(MPPA_DMA_DEVICE_HEADERS_PATH)/dma.coolidge-v*.dev $(STAGING_DIR)/usr/include/
-	$(INSTALL) -D -m 0644 $(MPPA_DMA_DEVICE_HEADERS_PATH)/gic.coolidge-v*.dev $(STAGING_DIR)/usr/include/
-	$(INSTALL) -D -m 0644 $(MPPA_DMA_DEVICE_HEADERS_PATH)/mailbox.coolidge-v*.dev $(STAGING_DIR)/usr/include/
-	$(INSTALL) -D -m 0644 $(MPPA_DMA_DEVICE_HEADERS_PATH)/interrupt_generator.coolidge-v*.dev $(STAGING_DIR)/usr/include/
-	$(INSTALL) -D -m 0644 $(MPPA_DMA_DEVICE_HEADERS_PATH)/pcie_dma_bridge.coolidge-v*.dev $(STAGING_DIR)/usr/include/
-	$(INSTALL) -D -m 0644 $(MPPA_DMA_DEVICE_HEADERS_PATH)/device_inttype.h $(STAGING_DIR)/usr/include/
-	$(INSTALL) -D -m 0755 $(@D)/output/lib/cluster/libmppa_dma.so $(STAGING_DIR)/usr/lib/libmppa_dma.so
+	$(INSTALL) -D -m 0644 $(@D)/include/mppa_dma.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/include/mppa_dma_pcie.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/include/arch/linux/mppa_dma_macros.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(MPPA_DMA_MACHINE_HEADERS_PATH)/device_inttype.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0755 $(@D)/output/lib/cluster/libmppa_dma.so $(STAGING_DIR)/usr/lib/
 endef
 
 define MPPA_DMA_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/output/lib/cluster/libmppa_dma.so $(TARGET_DIR)/usr/lib/libmppa_dma.so
+	$(INSTALL) -D -m 0755 $(@D)/output/lib/cluster/libmppa_dma.so $(TARGET_DIR)/usr/lib/
 endef
 
 $(eval $(generic-package))
