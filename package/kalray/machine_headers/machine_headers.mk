a44182a242f578053a2ca7d1fa1866df6fffc3ec
Julian Vetter <jvetter@kalrayinc.com>
2022-10-13 15:52:38 +0200

    mppa-dma: Fix issues with the build of mppa-dma
    Summary:
    The mppa-dma library takes the variable `DEVICE_HEADERS` to find the
    machine headers and not `MACHINE_HEADERS`. Also the dependency on
    machine_headers was missing on the mppa-dma library.
    
    Ref T18932
    
    Test Plan: CI
    
    Reviewers: ysionneau, O53 Buildroot Coolidge!
    
    Subscribers: etournier, #linux_coolidge_cc
    
    Maniphest Tasks: T18932
    
    Differential Revision: https://phab.kalray.eu/D24006

--- package/kalray/machine_headers/machine_headers.mk
+++ package/kalray/machine_headers/machine_headers.mk
@@ -10,7 +10,7 @@ MACHINE_HEADERS_INSTALL_STAGING = YES
 
 define MACHINE_HEADERS_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/usr/include/machine/devices
-	$(INSTALL) -D -m 0644 $(MACHINE_HEADERS_SITE)/* $(STAGING_DIR)/usr/include/machine/devices
+	$(INSTALL) -D -m 0644 $(@D)/* $(STAGING_DIR)/usr/include/machine/devices
 endef
 
 $(eval $(generic-package))
