cf7587723165adcfc75d2a9b876ade36361f7b69
jhascoet <jhascoet@kalray.eu>
2023-07-18 13:45:00 +0200

    pocl-firmware: fix target different than kv3-1
    Summary:
    We fix target firmware install for
    architecture different than kv3-1. Indeed,
    the default is CV1 located in /lib/firmware/kalray/opencl
    and other architectures add kvx-version_id architecture dir.
    
    Test Plan: CI
    
    Reviewers: O53 Buildroot Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D30563

--- package/kalray/pocl-firmware/pocl-firmware.mk
+++ package/kalray/pocl-firmware/pocl-firmware.mk
@@ -6,16 +6,21 @@
 
 POCL_FIRMWARE_VERSION = $(call qstrip,$(BR2_POCL_FIRMWARE_VERSION))
 POCL_FIRMWARE_SITE = $(call kalray,$(1),$(POCL_FIRMWARE_VERSION))
+POCL_FIRMWARE_PATH_PREFIX =
+ifneq ($(BR2_MARCH),kv3-1)
+POCL_FIRMWARE_PATH_PREFIX = $(BR2_MARCH)/
+endif
+POCL_FIRMWARE_PATH = $(TARGET_DIR)/lib/firmware/kalray/opencl/$(POCL_FIRMWARE_PATH_PREFIX)
 
 BR_NO_CHECK_HASH_FOR += $(POCL_FIRMWARE_SOURCE)
 
 define POCL_FIRMWARE_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/lib/firmware/kalray/opencl/
+	mkdir -p $(POCL_FIRMWARE_PATH)
 	if [ -z $(BR2_PACKAGE_POCL_FIRMWARE_LIST) ]; then \
-		cp -a $(@D)/*.elf $(TARGET_DIR)/lib/firmware/kalray/opencl/; \
+		cp -a $(@D)/$(POCL_FIRMWARE_PATH_PREFIX)*.elf $(POCL_FIRMWARE_PATH); \
 	else \
 		$(foreach fw, $(call qstrip,$(BR2_PACKAGE_POCL_FIRMWARE_LIST)), \
-			cp -a $(@D)/$(fw).elf $(TARGET_DIR)/lib/firmware/kalray/opencl/; \
+			cp -a $(@D)/$(POCL_FIRMWARE_PATH_PREFIX)$(fw).elf $(POCL_FIRMWARE_PATH); \
 		) \
 		true; \
 	fi
