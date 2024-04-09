3d5b6d9168fe7ca2de9c74c4cf488d08db43e68e
Guillaume Pommeret <gpommeret@kalrayinc.com>
2024-04-09 11:50:44 +0200

    phy-eth-firmware: firmware selection
    Summary:
    We only install the firmware that fits with the IP version
    present in the chip.
    
    Ref T22783
    
    Signed-off-by: Guillaume Pommeret <gpommeret@kalrayinc.com>
    
    Test Plan: CI
    
    Reviewers: O39 ethernet!, O53 Buildroot Coolidge!
    
    Subscribers: #linux_coolidge_cc
    
    Maniphest Tasks: T22783
    
    Differential Revision: https://phab.kalray.eu/D37365

--- package/kalray/phy-eth-firmware/phy-eth-firmware.mk
+++ package/kalray/phy-eth-firmware/phy-eth-firmware.mk
@@ -9,13 +9,9 @@ PHY_ETH_FIRMWARE_SITE = $(call kalray,$(1),$(PHY_ETH_FIRMWARE_VERSION))
 
 BR_NO_CHECK_HASH_FOR += $(PHY_ETH_FIRMWARE_SOURCE)
 
-define PHY_ETH_FIRMWARE_BUILD_CMDS
-	CROSS_COMPILE="$(TARGET_CROSS)" make -C $(@D) __build
-endef
-
 define PHY_ETH_FIRMWARE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/firmware/
-	cp -a $(@D)/*.bin $(TARGET_DIR)/lib/firmware/
+	cp -a $(@D)/$(BR2_MARCH)/*.bin $(TARGET_DIR)/lib/firmware/
 endef
 
 $(eval $(generic-package))
